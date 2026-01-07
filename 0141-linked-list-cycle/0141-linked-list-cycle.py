875448e068e7262a28f2b739526fb635ff961b5d
Hasika Cuddapah <hasika20.2005@gmail.com>
2026-01-07 11:31:46 +0530

    Time: 56 ms (9.74%), Space: 22.8 MB (8.56%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # Brute
+
+        temp = head
+        mpp = defaultdict(int)
+        while (temp):
+            if temp in mpp:
+                return True
+            mpp[temp] += 1
+            temp = temp.next
+        return False
