436d6950f0796d4df06b916683ff2cb85351fc70
Hasika Cuddapah <hasika20.2005@gmail.com>
2026-01-07 10:20:16 +0530

    Time: 0 ms (100%), Space: 20.5 MB (9.44%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,6 +5,8 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Brute
+
         # temp = head
         # stack = []
         # while temp:
@@ -18,13 +20,24 @@ class Solution:
         #     temp = temp.next
         # return head
 
-        prev = None
-        temp = head
-        while temp:
-            front = temp.next
-            temp.next = prev
-            prev = temp
-            temp = front
-        return prev
-            
+        # Optimal interative
+
+        # prev = None
+        # temp = head
+        # while temp:
+        #     front = temp.next
+        #     temp.next = prev
+        #     prev = temp
+        #     temp = front
+        # return prev
+        
+        # Optimal recursive
+
+        if not head or not head.next:
+            return head
+        new_head = self.reverseList(head.next)
+        front = head.next
+        front.next = head
+        head.next = None
+        return new_head
         
