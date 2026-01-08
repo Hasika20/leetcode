ddddfb7c738e2a987888202083e8f3a35ed146ff
Hasika Cuddapah <hasika20.2005@gmail.com>
2026-01-08 10:38:05 +0530

    Time: 46 ms (22.03%), Space: 42.4 MB (5.01%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -7,17 +7,41 @@ class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         # Brute
 
-        stack = []
+        # stack = []
+        # temp = head
+        # while temp:
+        #     stack.append(temp.val)
+        #     temp = temp.next
+        # temp = head
+        # while temp:
+        #     if temp.val != stack.pop():
+        #         return False
+        #     temp = temp.next
+        # return True
+
+        # Optimal
+
+        slow = head
+        fast = head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        prev = None
+        curr = slow.next
+        while curr:
+            front = curr.next
+            curr.next = prev
+            prev = curr
+            curr = front
         temp = head
-        while temp:
-            stack.append(temp.val)
-            temp = temp.next
-        temp = head
-        while temp:
-            if temp.val != stack.pop():
+        while prev:
+            if temp.val != prev.val:
                 return False
             temp = temp.next
+            prev = prev.next
         return True
+        
+        
 
 
         
